fc286bd0e153466ab5c3379e8cceb68d87998cd9
Georgy P <65073904+gr1spy@users.noreply.github.com>
2022-09-01 16:41:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,71 +4,82 @@ import os
 import logging
 import logging.handlers
 
-cmd = r'nltest /dclist:'
-
-# Домены, список которых проверяем
-sigma_domain = r'sigma.sbrf.ru'
-cib_domain = r'msk.trd.ru'
-delta_domain = r'delta.sbrf.ru'
-
-# Регулярки для доменов
-regex_sigma = r'[a-zA-Z0-9\-]+\.sigma\.sbrf\.ru'
-regex_cib = r'[a-zA-Z0-9\-]+\.msk\.trd\.ru'
-regex_delta = r'[a-zA-Z0-9\-]+\.delta\.sbrf\.ru'
+cmd = 'nltest /dclist:sigma.sbrf.ru & nltest /dclist:msk.trd.ru & nltest /dclist:delta.sbrf.ru'
+regex = [r'[a-zA-Z0-9\-]+\.sigma\.sbrf\.ru', r'[a-zA-Z0-9\-]+\.msk\.trd\.ru', r'[a-zA-Z0-9\-]+\.delta\.sbrf\.ru']
+path_to_script_dir = r'D:/dc_check_script/'
+old_num = ['']
+new_num = ['']
 
 # Адрес:порт для отправки Syslog
 SYSLOG_ADDRESS = '10.34.248.96'
 SYSLOG_PORT = 514
 
 
+# pre_query.txt - сохраняем текущий запрос по доменам
+# query.txt - записываем построчно DC из нового запроса
+# new_dc.txt - список новых DC
+# removed_dc.txt - список выведенных DC
+
 # Получаем новый список домен контролеров в домене
 def get_dc():
-    proc = subprocess.Popen(f'{cmd}{sigma_domain}', stdout=subprocess.PIPE, encoding='cp866')
-    with open('new_query.txt', 'w+') as output:
-        for line in proc.stdout:
-            if re.findall(regex_sigma, line):
-                result = re.findall(regex_sigma, line)
-                output.write(f'{result[0]}\n')
+    result = []
+    proc = subprocess.run(f'{cmd}', stdout=subprocess.PIPE, shell=True, encoding='utf-8')
+    with open(f'{path_to_script_dir}pre_query.txt', 'w') as output:
+        output.write(proc.stdout)
 
-    with open('new_query.txt', 'r') as source, open('query.txt', 'w') as dest:
-        dest.writelines(source.readlines()[1:])
+    with open(f'{path_to_script_dir}pre_query.txt', 'r') as output, open(f'{path_to_script_dir}query.txt', 'w') as output2:
+        for line in output.readlines():
+            for reg in regex:
+                if re.findall(reg, line):
+                    result += re.findall(reg, line)
+
+        result_new = list(set(result))
+        for dc in result_new:
+            output2.write(f'{dc}\n')
 
 
 def check_new_dc():
     files = open_files()
-    with open('new_dc_list.txt', 'w+') as output:
+    with open(f'{path_to_script_dir}new_dc.txt', 'w') as output:
         for num in files[1]:
             if num not in files[0]:
                 output.write(f'{num}\n')
 
-    if not (os.stat('new_dc_list.txt').st_size == 0):
-        with open('new_dc_list.txt', 'r') as f:
+    if not (os.stat(f'{path_to_script_dir}new_dc.txt').st_size == 0):
+        with open(f'{path_to_script_dir}new_dc.txt', 'r') as f:
             new_dc_to_syslog = [line.strip() for line in f]
         send_syslog(f'These domain controllers have been added: {new_dc_to_syslog}')
 
 
 def check_old_dc():
     files = open_files()
-    with open('removed_dc.txt', 'w+') as output:
+    with open(f'{path_to_script_dir}removed_dc.txt', 'w') as output:
         for num in files[0]:
             if num not in files[1]:
                 output.write(f'{num}\n')
 
-    if not (os.stat('removed_dc.txt').st_size == 0):
-        with open('removed_dc.txt', 'r') as f:
+    if not (os.stat(f'{path_to_script_dir}removed_dc.txt').st_size == 0):
+        with open(f'{path_to_script_dir}removed_dc.txt', 'r') as f:
             removed_dc_to_syslog = [line.strip() for line in f]
         send_syslog(f'These domain controllers have been removed: {removed_dc_to_syslog}')
 
 
 def open_files():
-    if os.path.isfile('last_query.txt'):
-        with open('last_query.txt', 'r') as f:
+    global old_num, new_num
+    if os.path.isfile(f'{path_to_script_dir}last_query.txt'):
+        with open(f'{path_to_script_dir}last_query.txt', 'r') as f:
             old_number = [line.strip() for line in f]
     else:
-        open('last_query.txt', 'w').close()
+        open(f'{path_to_script_dir}last_query.txt', 'w').close()
+        with open(f'{path_to_script_dir}last_query.txt', 'r') as f:
+            old_number = [line.strip() for line in f]
 
-    if os.path.isfile('query.txt'):
-        with open('query.txt', 'r') as f:
+    if os.path.isfile(f'{path_to_script_dir}query.txt'):
+        with open(f'{path_to_script_dir}query.txt', 'r') as f:
+            new_number = [line.strip() for line in f]
+    else:
+        open(f'{path_to_script_dir}query.txt', 'w').close()
+        with open(f'{path_to_script_dir}query.txt', 'r') as f:
             new_number = [line.strip() for line in f]
 
     return [old_number, new_number]
@@ -86,9 +97,9 @@ def send_syslog(dc_list):
 
 
 def rename_files():
-    os.remove('last_query.txt')
-    os.remove('new_query.txt')
-    os.renames('query.txt', 'last_query.txt')
+    os.remove(f'{path_to_script_dir}last_query.txt')
+    os.remove(f'{path_to_script_dir}pre_query.txt')
+    os.renames(f'{path_to_script_dir}query.txt', f'{path_to_script_dir}last_query.txt')
 
 
 def main():
