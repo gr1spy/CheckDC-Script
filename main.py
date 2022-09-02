37696320d93d94a4ba96238e13b72f7245291f2f
Georgy P <65073904+gr1spy@users.noreply.github.com>
2022-09-02 16:28:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import os
 import logging
 import logging.handlers
 
-cmd = 'nltest /dclist:sigma.sbrf.ru & nltest /dclist:msk.trd.ru & nltest /dclist:delta.sbrf.ru'
-regex = [r'[a-zA-Z0-9\-]+\.sigma\.sbrf\.ru', r'[a-zA-Z0-9\-]+\.msk\.trd\.ru', r'[a-zA-Z0-9\-]+\.delta\.sbrf\.ru']
+cmd = 'nltest /dclist:omega.sbrf.ru & nltest /dclist:ca.sbrf.ru'
+regex = [r'[a-zA-Z0-9\-\.]+\.sbrf\.ru']
 path_to_script_dir = r'D:/dc_check_script/'
 old_num = ['']
 new_num = ['']
 
 # Адрес:порт для отправки Syslog
-SYSLOG_ADDRESS = '10.34.248.96'
+SYSLOG_ADDRESS = '10.119.248.99'
 SYSLOG_PORT = 514
+
 
 # last_query.txt - Предыдущая выгрузка DC, с которой сравниваем текущую
 # new_dc.txt - список новых DC
@@ -21,11 +22,12 @@ SYSLOG_PORT = 514
 # Получаем новый список домен контролеров в домене
 def get_dc():
     result = []
-    proc = subprocess.run(f'{cmd}', stdout=subprocess.PIPE, shell=True, encoding='utf-8')
-    with open(f'{path_to_script_dir}pre_query.txt', 'w') as output:
-        output.write(proc.stdout)
+    proc = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
+    with open('%spre_query.txt' % path_to_script_dir, 'w', errors='ignore') as output:
+        output.write(proc.stdout.decode('cp850'))
 
-    with open(f'{path_to_script_dir}pre_query.txt', 'r') as output, open(f'{path_to_script_dir}query.txt', 'w') as output2:
+    with open('%spre_query.txt' % path_to_script_dir, 'r') as output, open('%squery.txt' % path_to_script_dir,
+                                                                           'w') as output2:
         for line in output.readlines():
             for reg in regex:
                 if re.findall(reg, line):
@@ -33,51 +35,51 @@ def get_dc():
 
         result_new = list(set(result))
         for dc in result_new:
-            output2.write(f'{dc}\n')
+            output2.write('%s\n' % dc)
 
 
 def check_new_dc():
     files = open_files()
-    with open(f'{path_to_script_dir}new_dc.txt', 'w') as output:
+    with open('%snew_dc.txt' % path_to_script_dir, 'w') as output:
         for num in files[1]:
             if num not in files[0]:
-                output.write(f'{num}\n')
+                output.write('%s\n' % num)
 
-    if not (os.stat(f'{path_to_script_dir}new_dc.txt').st_size == 0):
-        with open(f'{path_to_script_dir}new_dc.txt', 'r') as f:
+    if not (os.stat('%snew_dc.txt' % path_to_script_dir).st_size == 0):
+        with open('%snew_dc.txt' % path_to_script_dir, 'r') as f:
             new_dc_to_syslog = [line.strip() for line in f]
-        send_syslog(f'These domain controllers have been added: {new_dc_to_syslog}')
+        send_syslog(' CheckDCScript These domain controllers have been added: %s' % new_dc_to_syslog)
 
 
 def check_old_dc():
     files = open_files()
-    with open(f'{path_to_script_dir}removed_dc.txt', 'w') as output:
+    with open('%sremoved_dc.txt' % path_to_script_dir, 'w') as output:
         for num in files[0]:
             if num not in files[1]:
-                output.write(f'{num}\n')
+                output.write('%s}\n' % num)
 
-    if not (os.stat(f'{path_to_script_dir}removed_dc.txt').st_size == 0):
-        with open(f'{path_to_script_dir}removed_dc.txt', 'r') as f:
+    if not (os.stat('%sremoved_dc.txt' % path_to_script_dir).st_size == 0):
+        with open('%sremoved_dc.txt' % path_to_script_dir, 'r') as f:
             removed_dc_to_syslog = [line.strip() for line in f]
-        send_syslog(f'These domain controllers have been removed: {removed_dc_to_syslog}')
+        send_syslog(' CheckDCScript These domain controllers have been removed: 5s' % removed_dc_to_syslog)
 
 
 def open_files():
     global old_num, new_num
-    if os.path.isfile(f'{path_to_script_dir}last_query.txt'):
-        with open(f'{path_to_script_dir}last_query.txt', 'r') as f:
+    if os.path.isfile('%slast_query.txt' % path_to_script_dir):
+        with open('%slast_query.txt' % path_to_script_dir, 'r') as f:
             old_number = [line.strip() for line in f]
     else:
-        open(f'{path_to_script_dir}last_query.txt', 'w').close()
-        with open(f'{path_to_script_dir}last_query.txt', 'r') as f:
+        open('%slast_query.txt' % path_to_script_dir, 'w').close()
+        with open('%slast_query.txt' % path_to_script_dir, 'r') as f:
             old_number = [line.strip() for line in f]
 
-    if os.path.isfile(f'{path_to_script_dir}query.txt'):
-        with open(f'{path_to_script_dir}query.txt', 'r') as f:
+    if os.path.isfile('%squery.txt' % path_to_script_dir):
+        with open('%squery.txt' % path_to_script_dir, 'r') as f:
             new_number = [line.strip() for line in f]
     else:
-        open(f'{path_to_script_dir}query.txt', 'w').close()
-        with open(f'{path_to_script_dir}query.txt', 'r') as f:
+        open('%squery.txt' % path_to_script_dir, 'w').close()
+        with open('%squery.txt' % path_to_script_dir, 'r') as f:
             new_number = [line.strip() for line in f]
 
     return [old_number, new_number]
@@ -86,18 +88,20 @@ def open_files():
 def send_syslog(dc_list):
     log = logging.getLogger('dc')
     log.setLevel(logging.NOTSET)
-
+    formatter = logging.Formatter(fmt='%(asctime)-4s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
     syslog = logging.handlers.SysLogHandler(address=(SYSLOG_ADDRESS, SYSLOG_PORT))
     syslog.setLevel(logging.NOTSET)
+    syslog.setFormatter(formatter)
 
     log.addHandler(syslog)
     log.error(dc_list)
 
 
 def rename_files():
-    os.remove(f'{path_to_script_dir}last_query.txt')
-    os.remove(f'{path_to_script_dir}pre_query.txt')
-    os.renames(f'{path_to_script_dir}query.txt', f'{path_to_script_dir}last_query.txt')
+    os.remove('%slast_query.txt' % path_to_script_dir)
+    os.remove('%spre_query.txt' % path_to_script_dir)
+    os.renames('%squery.txt' % path_to_script_dir, '%slast_query.txt' % path_to_script_dir)
 
 
 def main():
